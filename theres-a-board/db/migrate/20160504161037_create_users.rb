b787cb97892b5f124ad1d6a6fe82c708d738d9f9
Jaclyn Feminella <email": null>
2016-05-04 14:21:34 -0400

    Initial commit with read me

---
+++ theres-a-board/db/migrate/20160504161037_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :full_name, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :photo_path
+      t.integer :phone_number
+      t.string :type, default: "Student"
+
+      t.timestamps null: false
+    end
+  end
+end
