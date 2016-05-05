7724ef9eea5d298a99ec2a122efd104da0dae0b9
Jaclyn Feminella <jaclynfeminella@Jaclyns-MacBook-Pro.local>
2016-05-05 13:50:53 -0400

    Add working routing between profile, appointment index, creation, and login/registration

--- theres-a-board/app/controllers/appointments_controller.rb
+++ theres-a-board/app/controllers/appointments_controller.rb
@@ -1,8 +1,11 @@
 class AppointmentsController < ApplicationController
 
+  def index
+    @appointments = Appointment.all
+  end
+
   def new
     @appointment = Appointment.new
-  
   end
 
   def create
