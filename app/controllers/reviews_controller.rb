d49ca077d66950db051b6299ebc0eac96933712f
Jaclyn Feminella <jaclynfeminella@Jaclyns-MacBook-Pro.local>
2016-05-06 12:33:15 -0400

    Work on seed file and adding reviews for user show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,6 +16,10 @@ class ReviewsController < ApplicationController
 
   end
 
+  # def show
+  #   @reviews = User.received_reviews
+  # end
+
    private
   def review_params
     params.require(:review).permit(:rating, :body)
